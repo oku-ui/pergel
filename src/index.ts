05a9b76feeaf6a5f62fef2a94c43728ffaf309f7
productdevbook <hi@productdevbook.com>
2023-10-06 10:48:18 +0300

    chore: add -v

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
 import { defineCommand, runMain } from 'citty'
+import packageJson from '../package.json'
 
 const main = defineCommand({
   meta: {
     name: 'Pergel',
-    version: '0.0.1',
+    version: packageJson.version,
     description: 'Pergel CLI',
+  },
+  args: {
+    version: {
+      alias: 'v',
+      description: 'Show version',
+    },
   },
   subCommands: {
     os: () => import('./commands/os').then(m => m.default),
     upgrade: () => import('./commands/upgrade').then(m => m.default),
   },
-  run() {
+  run({ args }) {
     console.warn('Pergel CLI')
-    console.warn('Use --help to see all commands')
+    if (args.version)
+      // eslint-disable-next-line ts/ban-ts-comment
+      // @ts-expect-error
+      console.warn('Pergel CLI version:', this.meta.version)
   },
 })
 
