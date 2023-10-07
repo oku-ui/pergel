8c97a7c6426713dec12e67364773ce421057da46
productdevbook <hi@productdevbook.com>
2023-10-07 14:38:15 +0300

    fix: upgrade

--- src/commands/upgrade.ts
+++ src/commands/upgrade.ts
@@ -9,19 +9,8 @@ export default defineCommand({
     version: '0.0.1',
   },
   async run() {
-    const githubRelease = execSync('curl -s https://api.github.com/repos/oku-ui/pergel/releases/latest').toString()
-
-    const githubReleaseJson = JSON.parse(githubRelease)
-    const githubReleaseVersion = githubReleaseJson.tag_name.replace('v', '')
-
-    if (githubReleaseVersion !== packageJson.version) {
-      console.warn(`New version available: ${githubReleaseVersion}`)
-      console.warn('Please wait...')
-      execSync('npm i -g pergel@latest', { stdio: 'inherit' })
-      console.warn('Done! ✅')
-    }
-    else {
-      console.warn('Already up to date!')
-    }
+    console.warn('Please wait...')
+    execSync('npm i -g pergel@latest', { stdio: 'inherit' })
+    console.warn('Done! ✅')
   },
 })
