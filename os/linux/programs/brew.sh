ab29363475e002788c61e2b3e3e154f98b962034
Eray Akgün <erayakgun1@outlook.com>
2023-10-13 08:57:42 +0300

    feat(ubuntu): brew installation option is added (#27)

---
+++ os/linux/programs/brew.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Installing Brew"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+sudo apt-get install build-essential
+brew install gcc
+echo "✅ Brew has been installed successfully."
+exec bash
+
