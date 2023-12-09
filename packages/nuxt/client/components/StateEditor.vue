c1c97ecdcc507cc469127f992064ec94bf0eef60
Mehmet <hi@productdevbook.com>
2023-12-09 17:16:29 +0300

    feat: add devtools and client build scripts (#48)
    * feat: add devtools and client build scripts
    
    * fix: getMyModuleOptions
    
    * fix: lint issues
    
    * feat: update okuPergel devtools configuration
    
    * Update baseURL and package names

---
+++ packages/nuxt/client/components/StateEditor.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import JsonEditorVue from 'json-editor-vue'
+
+const props = defineProps<{
+  name?: string
+  open?: boolean
+  state?: any
+  readonly?: boolean
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:open', value: boolean): void
+}>()
+
+const isOpen = useVModel(props, 'open', emit, { passive: true })
+const colorMode = useColorMode()
+const proxy = ref()
+
+const state = useState(props.name)
+if (props.state)
+  proxy.value = JSON.parse(JSON.stringify(props.state))
+else if (typeof props.state === 'number' || typeof props.state !== 'string')
+  proxy.value = props.state
+
+const watcher = watchPausable(
+  proxy,
+  (value) => {
+    if (typeof value !== 'number' && typeof value !== 'string')
+      deepSync(value, props.state)
+    else
+      state.value = value
+  },
+  { deep: true },
+)
+
+function deepSync(from: any, to: any) {
+  for (const key in from) {
+    if (from[key] === null)
+      to[key] = null
+    else if (Array.isArray(from[key]))
+      to[key] = from[key].slice()
+    else if (typeof from[key] === 'object')
+      deepSync(from[key], to[key])
+    else
+      to[key] = from[key]
+  }
+}
+
+async function refresh() {
+  watcher.pause()
+  proxy.value = JSON.parse(JSON.stringify(props.state))
+  await nextTick()
+  watcher.resume()
+}
+</script>
+
+<template>
+  <div class="state-editor-details" :open="!name ? true : isOpen">
+    <div flex="~ gap2" select-none items-center px4>
+      <button v-if="name" flex="~" cursor-pointer items-center :class="isOpen ? '' : 'op50'" @click="isOpen = !isOpen">
+        <div i-carbon-chevron-right transition :class="isOpen ? 'rotate-90 op0' : ''" />
+        <code px3 py1 font-mono :class="isOpen ? 'bg-[#8881] rounded-t' : 'rounded hover:bg-active'">{{ name }}</code>
+      </button>
+      <slot name="actions" v-bind="{ isOpen, name, state }" />
+      <template v-if="isOpen">
+        <NButton
+          v-tooltip.bottom="'Refresh View'" title="Refresh View" icon="carbon-renew" :border="false"
+          @click="refresh"
+        />
+        <DataSchemaButton v-if="proxy" :getter="() => ({ name, input: JSON.stringify(proxy) })" />
+      </template>
+    </div>
+    <template v-if="isOpen || !name">
+      <JsonEditorVue
+        v-model="proxy" v-bind="$attrs" class="json-editor-vue" :class="[
+          colorMode.value === 'dark' ? 'jse-theme-dark' : '',
+          name ? '' : '',
+        ]" :main-menu-bar="false" :navigation-bar="false" :status-bar="false" :read-only="readonly" :indentation="2"
+        :tab-size="2"
+      />
+    </template>
+  </div>
+</template>
--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -1,19 +1,51 @@
 import { addPlugin, createResolver, defineNuxtModule } from '@nuxt/kit'
-
-// Module options TypeScript interface definition
-export interface ModuleOptions {}
+import { extendServerRpc, onDevToolsInitialized } from '@nuxt/devtools-kit'
+import type { BirpcGroup } from 'birpc'
+import { setupDevToolsUI } from './devtools'
+import type { ModuleOptions } from './moduleType'
+import type { ClientFunctions, ServerFunctions } from './rpc-types'
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name: 'pergel',
+    configKey: 'pergel',
   },
   // Default configuration options of the Nuxt module
-  defaults: {},
-  setup(_options, _nuxt) {
+  defaults: {
+    devtools: true,
+    test: 'test',
+  },
+  async setup(options, nuxt) {
     const resolver = createResolver(import.meta.url)
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
     addPlugin(resolver.resolve('./runtime/plugin'))
+
+    let rpc: BirpcGroup<ClientFunctions, ServerFunctions> | undefined
+    if (options.devtools) {
+      setupDevToolsUI(nuxt, resolver)
+
+      const RPC_NAMESPACE = 'pergel-rpc'
+
+      const setupRpc = () => {
+        rpc = extendServerRpc<ClientFunctions, ServerFunctions>(RPC_NAMESPACE, {
+          // register server RPC functions
+          getMyModuleOptions() {
+            return options
+          },
+        })
+      }
+
+      try {
+        setupRpc()
+      }
+      catch (error) {
+        // wait for DevTools to be initialized
+        onDevToolsInitialized(async () => {
+          setupRpc()
+        })
+      }
+      await rpc?.broadcast.showNotification('Hello from Nuxt module!')
+    }
   },
 })
