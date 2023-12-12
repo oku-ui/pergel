ec931771c00fcd9a0b58daf6ada1d38b6c26dadb
productdevbook <hi@productdevbook.com>
2023-12-12 16:46:41 +0300

    chore: add changelogithub package

---
+++ changelogithub.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'changelogithub'
+
+// Config: https://github.com/antfu/changelogithub/blob/main/src/config.ts#L8
+export default defineConfig({
+  types: {
+    examples: { title: '🏀 Examples' },
+    version: { title: '🎉 Version' },
+  },
+})
