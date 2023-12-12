47c618bd3a400b86a65651fb0f565488057fb3b5
productdevbook <hi@productdevbook.com>
2023-12-12 17:11:14 +0300

    chore: release v0.2.0

--- changelogithub.config.ts
+++ changelogithub.config.ts
@@ -1,9 +1,6 @@
-import { defineConfig } from 'changelogithub'
-
-// Config: https://github.com/antfu/changelogithub/blob/main/src/config.ts#L8
-export default defineConfig({
+export default {
   types: {
     examples: { title: '🏀 Examples' },
     version: { title: '🎉 Version' },
   },
-})
+}
