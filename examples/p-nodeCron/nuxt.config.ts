dc6be881a8abc1c5f0129e39ffa0f906a476aa71
productdevbook <hi@productdevbook.com>
2023-12-12 16:10:12 +0300

    feat(examples): add nodeCron

---
+++ examples/p-nodeCron/nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@pergel/nuxt',
+  ],
+  pergel: {
+    projects: {
+      rocket: {
+        nodeCron: true,
+      },
+    },
+  },
+})
