c54be81e69a123229042430a6848d7206c2b5b07
productdevbook <hi@productdevbook.com>
2023-12-13 08:41:37 +0300

    chore: update dependencies and API documentation

--- examples/p-json2csv/app.vue
+++ examples/p-json2csv/app.vue
@@ -3,28 +3,11 @@ async function exportCSV() {
   await useFetch('/api/csvDownload', {
     method: 'get',
     onResponse({ response }) {
+      const { download } = usePergelJson2Csv()
+
       download(response._data, 'form.csv')
     },
   })
-}
-
-function download(data: any, filename: string) {
-  const defaultMimeType = 'application/octet-stream'
-  const blob = new Blob([data], { type: defaultMimeType })
-  const blobURL = window.URL.createObjectURL(blob)
-
-  const tempLink = document.createElement('a')
-  tempLink.style.display = 'none'
-  tempLink.href = blobURL
-  tempLink.setAttribute('download', filename)
-
-  if (typeof tempLink.download === 'undefined')
-    tempLink.setAttribute('target', '_blank')
-
-  document.body.appendChild(tempLink)
-  tempLink.click()
-  document.body.removeChild(tempLink)
-  window.URL.revokeObjectURL(blobURL)
 }
 </script>
 
