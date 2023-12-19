cb86a7838eb4962be6a2232a19643eb20ac9b430
productdevbook <hi@productdevbook.com>
2023-12-19 16:27:12 +0300

    examples: add graphqlYoga

--- examples/p-graphqlYoga/nuxt.config.ts
+++ examples/p-graphqlYoga/nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
@@ -7,7 +8,9 @@ export default defineNuxtConfig({
   pergel: {
     projects: {
       rocket: {
-        graphqlYoga: true,
+        graphqlYoga: {
+          mergeSchemas: true,
+        },
       },
     },
   },
