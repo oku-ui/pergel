586cc76b775ad5caa31261787c1f4f975d42136c
productdevbook <hi@productdevbook.com>
2023-12-22 16:06:06 +0300

    refactor(nuxt): user schema in postgresjs.ts

--- packages/nuxt/src/runtime/modules/drizzle/defaults/postgresjs.ts
+++ packages/nuxt/src/runtime/modules/drizzle/defaults/postgresjs.ts
@@ -6,10 +6,13 @@ export function createDrizzleConfig(data: {
 }) {
   const indexSchema = /* ts */`// Pergel auto generated please change this file as you wish
 import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core'
+// This pergel auth feature is not yet implemented. Do not use it. It will be implemented in the future.
 import { defaultUserSchema } from '@pergel/nuxt/drizzle/schema/pg'
 import { sql } from 'drizzle-orm'
 
-export const user = defaultUserSchema({})
+export const user = pgTable('user', {
+  ...defaultUserSchema,
+})
 
 export const hello = pgTable('hello', {
   id: uuid('id').primaryKey().default(sql\`uuid_generate_v4()\`),
--- packages/nuxt/src/runtime/modules/drizzle/templates/pg.ts
+++ packages/nuxt/src/runtime/modules/drizzle/templates/pg.ts
@@ -1,12 +1,9 @@
 import { sql } from 'drizzle-orm'
-import { type PgTableFn, pgTable, uuid, varchar } from 'drizzle-orm/pg-core'
+import { uuid, varchar } from 'drizzle-orm/pg-core'
 
-export function defaultUserSchema(column: Parameters<PgTableFn>[1]) {
-  return pgTable('user', {
-    id: uuid('id').primaryKey().default(sql`uuid_generate_v4()`),
-    email: varchar('email').unique().notNull(),
-    password: varchar('password').notNull(),
-    name: varchar('name').notNull(),
-    ...column,
-  })
+export const defaultUserSchema = {
+  id: uuid('id').primaryKey().default(sql`uuid_generate_v4()`),
+  email: varchar('email').unique().notNull(),
+  password: varchar('password').notNull(),
+  name: varchar('name').notNull(),
 }
