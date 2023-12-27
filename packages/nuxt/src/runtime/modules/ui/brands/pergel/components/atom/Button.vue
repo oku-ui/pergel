879b961047e23ae432c67060f4e61d876f227918
productdevbook <hi@productdevbook.com>
2023-12-27 18:04:49 +0300

    Add src/**/pergel to .gitignore

---
+++ packages/nuxt/src/runtime/modules/ui/brands/pergel/components/atom/Button.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { PrimitiveProps } from '@oku-ui/primitive'
+import { OkuPrimitive } from '@oku-ui/primitive'
+
+const { buttonVariants } = useVariants()
+interface Props extends PrimitiveProps {
+  variant?: NonNullable<Parameters<typeof buttonVariants>[0]>['variant']
+  size?: NonNullable<Parameters<typeof buttonVariants>[0]>['size']
+  as?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  as: 'button',
+})
+</script>
+
+<template>
+  <OkuPrimitive :as="as" :as-child="asChild" :class="cn(buttonVariants({ variant, size }), $attrs.class ?? '')">
+    <slot />
+  </OkuPrimitive>
+</template>
---
+++ packages/nuxt/src/runtime/modules/ui/brands/pergel/components/form/FormDescription.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+const { formDescriptionId } = useFormField()
+</script>
+
+<template>
+  <p
+    :id="formDescriptionId"
+    :class="cn('mt-2 text-sm text-gray-500', $attrs.class ?? '')"
+  >
+    <slot />
+  </p>
+</template>
