adc42e3853528829411416d04fad2cf0dd081a8e
productdevbook <hi@productdevbook.com>
2023-12-31 17:28:47 +0300

    Remove waitModule dependency in SES module

--- packages/nuxt/src/runtime/modules/ses/index.ts
+++ packages/nuxt/src/runtime/modules/ses/index.ts
@@ -10,7 +10,6 @@ export default definePergelModule({
     dependencies: {
       '@aws-sdk/client-ses': '^3.470.0',
     },
-    waitModule: ['nodeCron'],
   },
   defaults: {},
   async setup({ nuxt, moduleOptions }) {
