21ec069397eec6c5676503ade0dafeb2f6c29f32
productdevbook <hi@productdevbook.com>
2023-12-31 16:36:32 +0300

    Update package.json and auth-pages.mjs

---
+++ examples/p-lucia/server/api/ses.ts
@@ -0,0 +1,40 @@
+export default defineEventHandler(async (event) => {
+  try {
+    const { sendEmail, templates } = await pergelRocket().ses().use(event)
+
+    const _emailParams = {
+      Destination: {
+        ToAddresses: ['hi@productdevbook.com'],
+      },
+      Message: {
+        Body: {
+          Text: {
+            Data: 'Test email',
+          },
+        },
+        Subject: {
+          Data: 'Test email',
+        },
+      },
+      Source: 'noreply@productdevbook.com',
+    }
+
+    // const sendtest = await sendEmail(_emailParams)
+
+    const result = await sendEmail(templates().changeEmail({
+      to: 'hi@productdevbook.com',
+      code: '123456',
+      webUrl: 'https://productdevbook.com',
+      source: 'noreply@productdevbook.com',
+    }))
+
+    return {
+      data: {
+        mailResult: result,
+      },
+    }
+  }
+  catch (error: any) {
+    return error.message
+  }
+})
