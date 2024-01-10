7e3b33c177fbe037571d36a25a2ebf7c9ae53ec0
productdevbook <hi@productdevbook.com>
2024-01-10 07:08:32 +0300

    fix(nuxt): update default types to module.d.ts/.mts

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -18,8 +18,6 @@ import { setupPergel } from './runtime/core/setupPergel'
 import { generateReadmeYaml } from './runtime/core/utils/generateYaml'
 import { setupModules } from './runtime/core/setupModules'
 
-export type { PergelOptions }
-
 export default defineNuxtModule<PergelOptions>({
   meta: {
     name: DEVTOOLS_MODULE_NAME,
