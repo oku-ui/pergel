87986582acf762f5e6a1ef17d899ab953066d169
productdevbook <hi@productdevbook.com>
2024-02-03 18:57:20 +0300

    chore: update nuxt.config.ts and remove schema.graphql

--- themes/pergel-auth/nuxt.config.ts
+++ themes/pergel-auth/nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { join } from 'node:path'
+import {
+  CurrencyResolver,
+  DateTimeResolver,
+  JSONResolver,
+  NonEmptyStringResolver,
+  UUIDResolver,
+} from 'graphql-scalars'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
@@ -26,9 +35,34 @@ export default defineNuxtConfig({
             unsearch: true,
           },
         },
-        graphqlYoga: true,
+        lucia: {
+          driver: 'drizzle:postgre',
+        },
+        graphqlYoga: {
+          codegen: {
+            server: {
+              config({ dir }) {
+                return {
+                  enumValues: {
+                    RoleStatus: `${join(dir.server, 'drizzle-pzg', 'schema', 'user')}#RoleStatus`,
+                  },
+                  scalars: {
+                    DateTime: DateTimeResolver.extensions.codegenScalarType as any,
+                    UUID: UUIDResolver.extensions.codegenScalarType as any,
+                    JSON: JSONResolver.extensions.codegenScalarType as any,
+                    JSONObject: JSONResolver.extensions.codegenScalarType as any,
+                    NonEmptyString: NonEmptyStringResolver.extensions.codegenScalarType as any,
+                    Currency: CurrencyResolver.extensions.codegenScalarType as any,
+                  },
+                  enumsAsTypes: true,
+                  useTypeImports: true,
+                  strictScalars: true,
+                }
+              },
+            },
+          },
+        },
         drizzle: true,
-        lucia: true,
       },
     },
   },
