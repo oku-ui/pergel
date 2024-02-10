f68cb03f6ad5f4577b381733250e47fb93980b2f
productdevbook <hi@productdevbook.com>
2024-02-10 15:21:25 +0300

    Refactor tab generation in index.vue

--- packages/nuxt/client/components/modules/Drizzle.vue
+++ packages/nuxt/client/components/modules/Drizzle.vue
@@ -1,13 +1,17 @@
 <script lang="ts" setup>
-import { rpc } from '../../composables/rpc'
-import type { ResolvedDrizzleConfig } from '../../../src/runtime/modules/drizzle/types'
 import type { PergelModuleNames } from '../../../src/runtime/core/types/nuxtModule'
+import type { ResolvedGraphQLYogaConfig } from '../../../src/runtime/modules/graphqlYoga/types'
+import { rpc } from '../../composables/rpc'
+import type { TabStateInject } from '../TabGenerator.vue'
 
 const props = defineProps<{
   projectName: string
 }>()
 
-const tabs = reactive<{
+const emits = defineEmits<Emits>()
+const tabStateInject = inject('tabState') as Ref<TabStateInject>
+
+let tabs = reactive<{
   [key in string]: {
     name: string
     view: {
@@ -15,18 +19,23 @@ const tabs = reactive<{
     }
   }
 }>({})
-const selectedTab = ref('add')
 
-const moduleOptions = ref<ResolvedDrizzleConfig>()
+type Emits = {
+  selectedModule: [string]
+}
+
+const moduleOptions = ref<ResolvedGraphQLYogaConfig>()
 
 onMounted(async () => {
-  await getModuleOptios({
+  await getModuleOptions({
     projectName: props.projectName,
     module: 'drizzle',
   }) as any
 })
 
-async function getModuleOptios({ projectName, module }: { projectName: string, module: PergelModuleNames }) {
+tabs = tabStateInject?.value.getSecondaryContext() as any
+
+async function getModuleOptions({ projectName, module }: { projectName: string, module: PergelModuleNames }) {
   const data = await rpc.value?.getModuleOptions({
     moduleName: module,
     projectName,
@@ -34,119 +43,91 @@ async function getModuleOptios({ projectName, module }: { projectName: string, m
   moduleOptions.value = JSON.parse(JSON.stringify(data))
 }
 
-function clickPlugin(plugin: {
+function clickPlugin(input: {
   name: string
   endpoint: string
 }, external?: boolean) {
+  // @ts-ignore
   if (external) {
-    return window.open(plugin.endpoint, '_blank')
+    return window.open(input.endpoint, '_blank')
   }
   else {
-    tabs[plugin.name] = {
-      name: plugin.name,
+    tabs[input.name] = {
+      name: input.name,
       view: {
-        src: plugin.endpoint,
+        src: input.endpoint,
       },
     }
-    selectedTab.value = plugin.name
+    tabStateInject.value.saveSecondaryContext(tabs)
+
+    emits('selectedModule', input.name)
   }
 }
 </script>
 
 <template>
-  <div class="flex size-full flex-col">
-    <TabsRoot
-      v-model="selectedTab"
-      class="flex size-full flex-col overflow-scroll"
-      default-value="add"
-    >
-      <TabsList
-        class="flex"
-        aria-label="Manage your account"
+  <div
+    v-if="moduleOptions && tabStateInject?.tabState.value.activeTab === 'add'"
+    class="flex size-full grow flex-col rounded-b-md p-5 outline-none dark:bg-gray-950" value="add"
+  >
+    <template v-if="moduleOptions.plugins">
+      <NSectionBlock
+        v-for="plugin in Object.keys(moduleOptions.plugins)" :key="plugin" icon="carbon-plug" :text="plugin"
+        description="Active modules"
       >
-        <template v-if="tabs">
-          <TabsTrigger
-            v-for="tab in Object.values(tabs)"
-            :key="tab.name"
-            class="flex h-[30px] max-w-fit flex-1 cursor-default select-none items-center justify-center px-3 text-[10px] leading-none text-gray-900 outline-none first:rounded-tl-md last:rounded-tr-md hover:text-gray-500 data-[state=active]:text-gray-900 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-gray-500 dark:bg-gray-950 dark:text-gray-200 dark:hover:text-gray-400 dark:data-[state=active]:text-gray-400"
-            :value="tab.name"
+        <div class="grid grid-cols-3 gap-6">
+          <div>
+            <NButton class="h-10 w-full rounded-md bg-gray-600 text-white" @click="clickPlugin(plugin)">
+              Open Tab
+            </NButton>
+          </div>
+          <div>
+            <NButton class="h-10 w-full rounded-md bg-gray-600 text-white" @click="clickPlugin(plugin, true)">
+              Open External Tab
+            </NButton>
+          </div>
+        </div>
+      </NSectionBlock>
+    </template>
+    <NSectionBlock icon="carbon-plug" text="Plugins" description="Active modules">
+      <div class="grid grid-cols-3 gap-6">
+        <div>
+          <NButton
+            class="h-10 w-full rounded-md bg-gray-600 text-white" @click="clickPlugin({
+              name: 'drizzle-studio',
+              endpoint: 'https://local.drizzle.studio/?port=3105',
+            })"
           >
-            {{ tab.name }}
-            <button
-              class="i-ph-x size-3 dark:text-white"
-              @click="() => {
-                delete tabs[tab.name]
-                selectedTab = 'add'
-              }"
-            >
-            </button>
-          </TabsTrigger>
-        </template>
-        <TabsTrigger
-          class="flex h-[30px] max-w-fit flex-1 cursor-default select-none items-center justify-center px-3 text-[10px] leading-none text-gray-900 outline-none first:rounded-tl-md last:rounded-tr-md hover:text-blue-500 data-[state=active]:text-blue-900 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-blue-500 dark:bg-gray-950 dark:text-gray-200 dark:hover:text-blue-400 dark:data-[state=active]:text-blue-400
-           "
-          value="add"
-        >
-          <div class="i-ph-plus size-3 dark:text-white" />
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent
-        v-if="moduleOptions"
-        class="flex size-full grow flex-col rounded-b-md p-5 outline-none dark:bg-gray-950"
-        value="add"
-      >
-        <template
-          v-if="moduleOptions"
-        >
-          <NSectionBlock
-            icon="carbon-plug"
-            text="Plugins"
-            description="Active modules"
+            Open Tab
+          </NButton>
+        </div>
+        <div>
+          <NButton
+            class="h-10 w-full rounded-md bg-gray-600 text-white" @click="clickPlugin({
+              name: 'drizzle-studio',
+              endpoint: 'https://local.drizzle.studio/?port=3105',
+            }, true)"
           >
-            <div class="grid grid-cols-3 gap-6">
-              <div>
-                <NButton
-                  class="h-10 w-full rounded-md bg-gray-600 text-white"
-                  @click="clickPlugin({
-                    name: 'drizzle-studio',
-                    endpoint: 'https://local.drizzle.studio/?port=3105',
-                  })"
-                >
-                  Open Tab
-                </NButton>
-              </div>
-              <div>
-                <NButton
-                  class="h-10 w-full rounded-md bg-gray-600 text-white"
-                  @click="clickPlugin({
-                    name: 'drizzle-studio',
-                    endpoint: 'https://local.drizzle.studio/?port=3105',
-                  }, true)"
-                >
-                  Open External Tab
-                </NButton>
-              </div>
-            </div>
-          </NSectionBlock>
-        </template>
-      </TabsContent>
-      <TabsContent
-        v-for="tab in Object.values(tabs)"
-        :key="tab.name"
-        :value="tab.name"
-        class="flex size-full grow flex-col rounded-b-md outline-none dark:bg-gray-950"
-      >
-        <IframeView
-          v-if="tab.view.src"
-          class="flex size-full grow flex-col rounded-b-md outline-none dark:bg-gray-950"
-          :tab="{
-            name: `${tab.name}drizzle`,
-            view: {
-              src: tab.view.src,
-            },
-          }"
-        />
-      </TabsContent>
-    </TabsRoot>
+            Open External Tab
+          </NButton>
+        </div>
+      </div>
+    </NSectionBlock>
   </div>
+
+  <template v-for="tab in Object.values(tabs)">
+    <div
+      v-if="tab.view.src && tabStateInject?.tabState.value?.activeTab === tab.name" :key="tab.name" :value="tab.name"
+      class="flex size-full grow flex-col rounded-b-md outline-none dark:bg-gray-950"
+    >
+      <IframeView
+        class="flex grow flex-col rounded-b-md outline-none dark:bg-gray-950" :tab="{
+          name: `${tab.name}graphqlYoga`,
+          view: {
+            src: tab.view.src,
+          },
+        }"
+      />
+    </div>
+  </template>
 </template>
--- packages/nuxt/client/pages/index.vue
+++ packages/nuxt/client/pages/index.vue
@@ -162,102 +162,31 @@ function clickRemoveTab(tab: string) {
       >
       </ModulesGraphqlYoga>
     </TabGenerator>
+
+    <TabGenerator
+      v-if="tabState.activeTab.includes('.drizzle')"
+      v-slot="{ selectModule }"
+      :project-name="tabState.activeTab.split('.')[0]"
+      :module-name="tabState.activeTab.split('.')[1]"
+    >
+      <ModulesDrizzle
+        :project-name="tabState.activeTab.split('.')[0]"
+        @selected-module="selectModule"
+      >
+      </ModulesDrizzle>
+    </TabGenerator>
+
+    <TabGenerator
+      v-if="tabState.activeTab.includes('.S3')"
+      v-slot="{ selectModule }"
+      :project-name="tabState.activeTab.split('.')[0]"
+      :module-name="tabState.activeTab.split('.')[1]"
+    >
+      <ModulesAssetS3
+        :project-name="tabState.activeTab.split('.')[0]"
+        @selected-module="selectModule"
+      >
+      </ModulesAssetS3>
+    </TabGenerator>
   </div>
 </template>
-
-<!-- <template>
-  <div class="flex size-full flex-col">
-    <TabsRoot
-      v-model="selectedTab"
-      class="flex size-full flex-col"
-      default-value="add"
-    >
-      <TabsList
-        class="flex"
-        aria-label="Manage your account"
-      >
-        <template v-if="tabs && tabs.length">
-          <TabsTrigger
-            v-for="tab in tabs"
-            :key="tab"
-            class="flex h-[30px] max-w-fit flex-1 cursor-default select-none items-center justify-center px-3 text-[10px] leading-none text-gray-900 outline-none first:rounded-tl-md last:rounded-tr-md hover:text-gray-500 data-[state=active]:text-gray-900 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-gray-500 dark:bg-gray-950 dark:text-gray-200 dark:hover:text-gray-400 dark:data-[state=active]:text-gray-400"
-            :value="tab"
-          >
-            {{ tab }}
-
-            <button
-              class="i-ph-x ml-1 size-3 dark:text-white"
-              @click="() => {
-                tabs.splice(tabs.indexOf(tab), 1)
-                selectedTab = tabs[tabs.length - 1]
-              }"
-            />
-          </TabsTrigger>
-        </template>
-        <TabsTrigger
-          class="flex h-[30px] max-w-fit flex-1 cursor-default select-none items-center justify-center px-3 text-[10px] leading-none text-gray-900 outline-none first:rounded-tl-md last:rounded-tr-md hover:text-blue-500 data-[state=active]:text-blue-900 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-blue-500 dark:bg-gray-950 dark:text-gray-200 dark:hover:text-blue-400 dark:data-[state=active]:text-blue-400
-           "
-          value="add"
-        >
-          <div class="i-ph-plus size-3 dark:text-white" />
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent
-        v-if="projects"
-        class="flex size-full grow flex-col rounded-b-md p-5 outline-none dark:bg-gray-950"
-        value="add"
-      >
-        <template
-          v-if="projects"
-        >
-          <NSectionBlock
-            v-for="project in Object.keys(projects)"
-            :key="project"
-            icon="carbon-plug"
-            :text="project"
-            :description="`Active modules: ${projects.length ?? 0}/${(totalModules && totalModules.length) ?? 0}`"
-          >
-            <div class="grid grid-cols-4 gap-6">
-              <template
-                v-for="module in Object.keys(projects[project])"
-                :key="module.id"
-              >
-                <button
-                  class="h-10 w-full rounded-md bg-gray-600 text-white"
-                  @click="() => {
-                    tabs.push(`${project}.${module}`)
-                    selectedTab = `${project}.${module}`
-                  }"
-                >
-                  {{ module }}
-                </button>
-              </template>
-            </div>
-          </NSectionBlock>
-        </template>
-      </TabsContent>
-
-      <TabsContent
-        v-for="tab in tabs"
-        :key="tab"
-        :value="tab"
-        class="flex size-full grow flex-col rounded-b-md outline-none dark:bg-gray-950"
-      >
-        <ModulesAssetS3
-          v-if="tab.includes('.S3')"
-          :project-name="tab.split('.')[0]"
-        />
-        <ModulesGraphqlYoga
-          v-if="tab.includes('.graphqlYoga')"
-          :project-name="tab.split('.')[0]"
-        >
-        </ModulesGraphqlYoga>
-        <ModulesDrizzle
-          v-if="tab.includes('.drizzle')"
-          :project-name="tab.split('.')[0]"
-        >
-        </ModulesDrizzle>
-      </TabsContent>
-    </TabsRoot>
-  </div>
-</template> -->
