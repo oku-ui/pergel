82b9a02fc19021df0f1d620ead250c6bcc0ba6e9
productdevbook <hi@productdevbook.com>
2024-02-17 12:10:01 +0300

    fix(nuxt): remove unused import from urql module

--- packages/nuxt/src/runtime/modules/urql/index.ts
+++ packages/nuxt/src/runtime/modules/urql/index.ts
@@ -119,9 +119,10 @@ export default ${options.projectNameCamelCaseWithPergel}UrqlClient((ssr) => {
       filename: `${options.projectName}.urql.mjs`,
       write: true,
       async getContents() {
-        return /* ts */`import { ref, defineNuxtPlugin, useState, usePergelRuntime } from "#imports"
+        return /* ts */`import { ref, defineNuxtPlugin, useState } from "#imports"
 import { createClient, ssrExchange } from "@urql/core";
 import nuxtURQLClient from '#urql-client'
+import { usePergelRuntime } from '@pergel/nuxt/dist/runtime/core/utils/usePergelRuntime.mjs'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   const { selectProject } = usePergelRuntime({
