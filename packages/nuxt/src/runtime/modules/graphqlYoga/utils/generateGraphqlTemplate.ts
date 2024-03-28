b5e225a2385ffed1778802292d4d455fbe64c31a
Daniel Roe <daniel@roe.dev>
2024-03-28 05:16:46 +0000

    fix: adopt forward-compatible approach to `builder:watch` (#247)
    * fix: adopt forward-compatible approach to `builder:watch`
    
    * fix: lint issues
    
    * Apply suggestions from code review

--- packages/nuxt/src/runtime/modules/graphqlYoga/utils/generateGraphqlTemplate.ts
+++ packages/nuxt/src/runtime/modules/graphqlYoga/utils/generateGraphqlTemplate.ts
@@ -1,4 +1,4 @@
-import { join } from 'node:path'
+import { join, relative, resolve } from 'node:path'
 import { matchGlobs } from '../utils'
 import type { ResolvedGraphQLYogaConfig } from '../types'
 import type { NuxtPergel } from '../../../core/types/nuxtModule'
@@ -22,6 +22,7 @@ export function generateGraphQLTemplate(data: {
 
   if (data.nuxt.options.dev) {
     data.nuxt.hook('builder:watch', async (event, path) => {
+      path = relative(data.nuxt.options.srcDir, resolve(data.nuxt.options.srcDir, path))
       const test = globsBuilderWatch(data.nuxt, path, '.graphql')
       if (!test)
         return
